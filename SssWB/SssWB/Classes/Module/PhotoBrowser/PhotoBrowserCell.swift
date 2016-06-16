0eb1313719c3363926a759fc751b8eae1dc93ba7
yanbo <yanbo@ybPro.local>
2016-06-16 15:02:27 +0800

    照片选择器

--- SssWB/SssWB/Classes/Module/PhotoBrowser/PhotoBrowserCell.swift
+++ SssWB/SssWB/Classes/Module/PhotoBrowser/PhotoBrowserCell.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 import SDWebImage
-
+import SVProgressHUD
 
 class PhotoBrowserCell: UICollectionViewCell {
     
     var url : NSURL? {
+        
         didSet {
+            indicatorView.startAnimating()
             imageView.sd_setImageWithURL(url, placeholderImage: nil, options: [SDWebImageOptions.RetryFailed]) { (image, error, _, _) in
-                    self.imageView.sizeToFit()
+                
+                self.indicatorView.stopAnimating()
+                if error != nil {
+                    SVProgressHUD.showWithStatus("你麻痹,网络不给力啊")
+                    return
+                }
+                
+                self.setImagePosition()
             }
         }
     }
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,12 +44,56 @@ class PhotoBrowserCell: UICollectionViewCell {
         // 1. 添加控件
         contentView.addSubview(scrollView)
         scrollView.addSubview(imageView)
-        
+        contentView.addSubview(indicatorView)
         // 2. 设置约束
-        scrollView.frame = bounds
+        var rect = bounds
+        rect.size.width -= 20
+        scrollView.frame  = rect
+        indicatorView.center = scrollView.center
+        prepareScrollView()
+    }
+    
+    private func prepareScrollView(){
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.5
+        scrollView.maximumZoomScale = 2.0
         
         
     }
+    
+    private func setImagePosition(){
+        
+        let size = displaySize(imageView.image!)
+        
+        imageView.frame = CGRect(origin: CGPointZero, size: size)
+        if size.height < scrollView.bounds.height {
+            let y = (scrollView.bounds.height - size.height) * 0.5
+            
+            scrollView.contentInset = UIEdgeInsets(top: y, left: 0, bottom: 0, right: 0)
+        } else {
+            // 长图
+            scrollView.contentSize = size
+        }
+        
+    }
+    
+    ///  根据图像大小调整图像位置
+    ///
+    ///  - parameter image: 图片
+    ///
+    ///  - returns: 位置
+    private func displaySize(image:UIImage) -> CGSize {
+        // 图像宽高比
+        let scale = image.size.height / image.size.width
+        
+        let w = scrollView.bounds.width
+        
+        let h = w * scale
+        
+        return CGSize(width: w, height: h)
+        
+    }
+    
     
     /// 滚动试图
     private lazy var scrollView : UIScrollView = UIScrollView()
@@ -47,4 +101,30 @@ class PhotoBrowserCell: UICollectionViewCell {
     ///  单张图片
     private lazy var imageView : UIImageView = UIImageView()
     
+    ///  指示器
+    private lazy var indicatorView  = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
+    
 }
+// MARK: - UIScrollViewDelegate
+extension PhotoBrowserCell:UIScrollViewDelegate {
+    // 缩放的试图
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+        var offectY = (scrollView.bounds.height - imageView.frame.height) * 0.5
+        var offectX = (scrollView.bounds.height - imageView.frame.height) * 0.5
+
+        offectY = (offectY < 0) ? 0 : offectY
+        offectX = (offectX < 0) ? 0 : offectX
+//        let offectX = scrollView
+        
+        scrollView.contentInset = UIEdgeInsets(top: offectY
+            , left: offectX, bottom: 0, right: 0 )
+        
+    }
+    
+}
+
+
--- SssWB/SssWB/Classes/Module/PhotoBrowser/PhotoBrowserViewController.swift
+++ SssWB/SssWB/Classes/Module/PhotoBrowser/PhotoBrowserViewController.swift
@@ -36,10 +36,18 @@ class PhotoBrowserViewController: UIViewController {
         printLog(selectedIndexPath)
         
     }
+    ///  完成界面布局
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        /// 滚动到点击试图
+        collectionView.scrollToItemAtIndexPath(selectedIndexPath, atScrollPosition: UICollectionViewScrollPosition.Left, animated: false)
+        pageControl.currentPage = selectedIndexPath.item
+    }
     
     // MARK: - 设置界面
     override func loadView() {
-        let rect = UIScreen.mainScreen().bounds
+        var rect = UIScreen.mainScreen().bounds
+        rect.size.width += 20
         view = UIView(frame: rect)
         view.backgroundColor = UIColor.blackColor()
         
@@ -55,11 +63,13 @@ class PhotoBrowserViewController: UIViewController {
         view.addSubview(collectionView)
         view.addSubview(saveButton)
         view.addSubview(closeBtn)
+        view.addSubview(pageControl)
         // 2. 自动布局
         collectionView.frame = view.bounds
         
         saveButton.translatesAutoresizingMaskIntoConstraints = false
         closeBtn.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
         
         let viewDict = ["sa":saveButton,"cl":closeBtn]
         // 水平方向
@@ -68,8 +78,11 @@ class PhotoBrowserViewController: UIViewController {
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[cl(35)]-8-|", options: [], metrics: nil, views: viewDict))
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[sa(35)]-8-|", options: [], metrics: nil, views: viewDict))
         
-        prepareCollectionView()
+        view.addConstraint(NSLayoutConstraint(item: pageControl, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: pageControl, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: -8))
         
+        prepareCollectionView()
+        preparePageControl()
         // 3 监听方法
         closeBtn.rac_signalForControlEvents(.TouchUpInside).subscribeNext { [weak self](btn) in
             
@@ -97,27 +110,41 @@ class PhotoBrowserViewController: UIViewController {
         
     }
     
+    private func preparePageControl(){
+        pageControl.numberOfPages = urls.count
+        pageControl.hidesForSinglePage = true
+        pageControl.pageIndicatorTintColor = UIColor.whiteColor()
+        pageControl.currentPageIndicatorTintColor = UIColor.redColor()
+        
+    }
+    
     // MARK: - 懒加载控件
     /// 照片查看视图
     private lazy var collectionView : UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
     ///  保存按钮
     private lazy var saveButton :UIButton = UIButton(title: "保存", fontSize: 14)
-    
     ///  关闭按钮
     private lazy var closeBtn : UIButton = UIButton(title: "关闭", fontSize: 14)
+    /// 分页控件
+    private lazy var pageControl : UIPageControl = UIPageControl()
     
 }
 
 extension PhotoBrowserViewController : UICollectionViewDelegate,UICollectionViewDataSource {
+    // indexPath 是前一个cell的索引
+    func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        pageControl.currentPage = collectionView.indexPathsForVisibleItems().last?.item ?? 0
+    }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.urls.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(PhotoBrowserCellID, forIndexPath: indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(PhotoBrowserCellID, forIndexPath: indexPath) as! PhotoBrowserCell
         
-        cell.backgroundColor = UIColor.randomColor()
+        cell.backgroundColor = UIColor.blackColor()
+        cell.url = urls[indexPath.item]
         return cell
     }
     
